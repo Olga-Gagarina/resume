c9e688475eb6bfb40de37dc95760d732c957b39f
Hanna Kushnir <62173987+anna-kushnir@users.noreply.github.com>
2023-05-31 16:50:35 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -1553,225 +1553,6 @@ router.get('/shophome', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/shopproduct', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('shopproduct', {
-    layout: 'shop',
-    navigation: {
-      links: [
-        {
-          text: 'Home',
-          href: 'https://github.com/',
-        },
-        {
-          text: 'Contacts',
-          href: 'https://www.google.com/',
-        },
-        {
-          text: 'Help',
-          href: 'https://www.youtube.com/',
-        },
-      ],
-    },
-
-    breadcrumb: [
-      { name: 'Home', url: 'https://github.com/' },
-      { name: 'PC', url: 'https://www.google.com/' },
-      { name: 'Windows', url: 'https://github.com/' },
-      { name: 'Product PC #3123', url: null },
-    ],
-
-    productData: {
-      img: 'https://picsum.photos/400/200',
-
-      info: {
-        title: 'Product PC Asus 331 BC 671',
-        about:
-          "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        price: '1000$',
-
-        category: [
-          { name: 'HOT', isHot: true },
-          { name: 'New', isNew: true },
-          { name: 'Bonus', isBonus: true },
-        ],
-      },
-
-      actionList: [
-        { name: 'Buy', isWarning: true },
-        { name: 'Add to cart', isError: true },
-        { name: 'Like', isSuccess: true },
-        { name: 'Share', isGood: true },
-      ],
-
-      item: {
-        head: ['#', 'color', 'price'],
-
-        body: [
-          ['id312', 'red', '1100$'],
-          ['id532', 'blue', '1050$'],
-          ['id643', 'dark', '999$'],
-        ],
-      },
-      
-      param: {
-        title: 'Product params',
-        list: ['Экран 15.6', 'IPS (1920x1080)', 'Full HD'],
-      },
-
-      alert: {
-        isError: true,
-        text: 'Danger! This is a danger alert — check it out!',
-      },
-      
-    },
-
-    goodsOtherBlock: {
-      title: 'See other products',
-      cards: [
-        {
-          image: 'https://picsum.photos/400/200',
-          title: 'iPhone 13',
-          description:
-            'The latest iPhone model features a new A15 Bionic chip, improved camera system, and longer battery life.',
-          isHot: false,
-          idNew: false,
-        },
-        {
-          image: 'https://picsum.photos/400/200',
-          title: 'MacBook Pro',
-          description:
-            "Apple's high-end laptop features a 16-inch Retina display, powerful M1 Pro or M1 Max chip, and up to 64GB of RAM.",
-          isHot: true,
-          idNew: false,
-        },
-        {
-          image: 'https://picsum.photos/400/200',
-          title: 'AirPods Pro',
-          description:
-            "Apple's premium wireless earbuds feature active noise cancellation, a customizable fit, and up to 4.5 hours of listening time.",
-          isHot: false,
-          idNew: false,
-        },
-      ],
-    },
-
-    service: {
-      title: 'Our Services',
-      description:
-        'We offer a variety of services to meet your needs, including web design, content creation, and social media management.',
-      buttons: [
-        {
-          text: 'Show More',
-          link: 'https://www.youtube.com/',
-          rel: 'show-more',
-        },
-      ],
-    },
-
-    footer: [
-      [
-        {
-          text: 'Home',
-          link: 'https://www.youtube.com/',
-        },
-        {
-          text: 'Trending',
-          link: 'https://www.youtube.com/feed/trending',
-        },
-        {
-          text: 'Subscriptions',
-          link: 'https://www.youtube.com/feed/subscriptions',
-        },
-        {
-          text: 'Library',
-          link: 'https://www.youtube.com/feed/library',
-        },
-      ],
-      [
-        {
-          text: 'History',
-          link: 'https://www.youtube.com/feed/history',
-        },
-        {
-          text: 'Your Videos',
-          link: 'https://www.youtube.com/feed/my_videos',
-        },
-        {
-          text: 'Live',
-          link: 'https://www.youtube.com/live',
-        },
-        {
-          text: 'Settings',
-          link: 'https://www.youtube.com/account',
-        },
-      ],
-      [
-        {
-          text: 'Watch Later',
-          link: 'https://www.youtube.com/playlist?list=WL',
-        },
-        {
-          text: 'Liked Videos',
-          link: 'https://www.youtube.com/playlist?list=LL',
-        },
-        {
-          text: 'Music',
-          link: 'https://www.youtube.com/music',
-        },
-        {
-          text: 'Gaming',
-          link: 'https://www.youtube.com/gaming',
-        },
-      ],
-      [
-        {
-          text: 'Sports',
-          link: 'https://www.youtube.com/channel/UCo_q6aOlvPH7M-j_XGWVgXg',
-        },
-        {
-          text: 'News',
-          link: 'https://www.youtube.com/news',
-        },
-        {
-          text: 'Fashion & Beauty',
-          link: 'https://www.youtube.com/channel/UC1x8rV_f-2yPpzlN0JWZXIQ',
-        },
-        {
-          text: 'Learning',
-          link: 'https://www.youtube.com/learning',
-        },
-      ],
-      [
-        {
-          text: 'Report History',
-          link: 'https://www.youtube.com/feed/history/report_history',
-        },
-        {
-          text: 'Help',
-          link: 'https://support.google.com/youtube/?hl=en',
-        },
-        {
-          text: 'Send Feedback',
-          link: 'https://support.google.com/youtube/answer/4347644?hl=en',
-        },
-        {
-          text: 'About',
-          link: 'https://www.youtube.com/about/',
-        },
-      ],
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
-// router.get Створює нам один ентпоїнт
-
-//           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/shoporder', function (req, res) {
   // res.render генерує нам HTML сторінку
 
@@ -2392,12 +2173,12 @@ router.get('/shopcart', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/shopprofile', function (req, res) {
+router.get('/shopreview', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('shopprofile', {
-    layout: 'shop',
+  res.render('shopreview', {
+    // layout: 'shop',
     navigation: {
       links: [
         {
@@ -2414,177 +2195,171 @@ router.get('/shopprofile', function (req, res) {
         },
       ],
     },
-
-    header: 'Мої замовлення',
-
-    purchase: {
-      tabs: ['Всі замовлення', 'Гарантія та повернення'],
+    formBlock: {
+      header: 'Відгуки покупців про Ноут',
+      rating: {
+        title: 'Оцінка',
+        value: '4.5/5',
+      },
       actionBlock: {
-        input: { placeholder: 'Placeholder' },
+        tabs: [
+          { text: 'Про товар' },
+          { text: 'Характеристики' },
+          { text: 'Відгуки', isActive: true },
+        ],
+        report: {
+          title: 'Залиште відгук про товар',
+          button: {
+            text: 'Написати відгук',
+            link: 'https://www.youtube.com/',
+          },
+        },
+      },
+      feedbackBlock: {
+        title:
+          'Хочете поділитись своєю думкою про наш магазин?',
         buttons: [
           {
-            text: 'Знайти',
+            text: 'Додати відгук',
+            isWarning: true,
             link: 'https://www.youtube.com/',
+          },
+          {
+            text: 'Позитивні відгуки',
             isSuccess: true,
+            link: 'https://www.youtube.com/',
           },
           {
-            text: 'Виділити все',
-            link: 'https://www.youtube.com/',
-            isOutline: true,
-          },
-          {
-            text: 'Очистити все',
-            link: 'https://www.youtube.com/',
+            text: 'Негативні відгуки',
             isDanger: true,
+            link: 'https://www.youtube.com/',
           },
         ],
       },
-      itemList: [
-        {
-          number: '№12587463 від 01.01.2023',
-          status: 'Виконано',
-          amount: { title: 'Сума', value: '25 000 ₴' },
-          images: ['https://picsum.photos/110/100'],
-          button: {
-            text: 'Детальніше',
-            link: 'https://www.youtube.com/',
+      goodsInfo: {
+        photo: 'https://picsum.photos/110/100',
+        name: 'Ноут Asus',
+        price: '25 000 ₴',
+        inMarket: true,
+        buttons: [
+          {
+            text: 'Купити',
             isPrimary: true,
-          },
-        },
-        {
-          number: '№12587463 від 01.01.2023',
-          status: 'Виконано',
-          amount: { title: 'Сума', value: '25 000 ₴' },
-          images: [
-            'https://picsum.photos/110/100',
-            'https://picsum.photos/110/100',
-            'https://picsum.photos/110/100',
-          ],
-          button: {
-            text: 'Детальніше',
             link: 'https://www.youtube.com/',
-            isPrimary: true,
           },
-        },
-        {
-          number: '№12587463 від 01.01.2023',
-          status: 'Виконано',
-          amount: { title: 'Сума', value: '25 000 ₴' },
-          images: [
-            'https://picsum.photos/110/100',
-            'https://picsum.photos/110/100',
-          ],
-          button: {
-            text: 'Детальніше',
+          {
+            text: 'Купити в кредит',
+            isSecondary: true,
             link: 'https://www.youtube.com/',
-            isPrimary: true,
           },
-        },
-      ],
-    },
-    userInfo: {
-      title: 'Особиста інформація',
-      profileData: {
-        title: 'Особисті дані',
-        fullName: {
-          surname: {
-            title: 'Прізвище',
-            value: 'Іванов',
-          },
-          name: {
-            title: 'Ім’я',
-            value: 'Іван',
-          },
-          middleName: {
-            title: 'По-батькові',
-            value: 'Іванович',
-          },
-        },
-        otherInfo: {
-          birthday: {
-            title: 'Дата народження',
-            value: '01.01.2000',
-          },
-          sex: {
-            title: 'Стать',
-            value: 'Чоловіча',
-          },
-          language: {
-            title: 'Мова',
-            value: 'Українська',
-          },
+        ],
+        seller: {
+          title: 'Продавець',
+          value: 'Machinery',
         },
       },
+    },
+
+    reviewList: [
+      {
+        title: {
+          userName: 'Іван Іванов',
+          caption: 'Відгук покупця',
+          date: '1 січня 2023',
+        },
+        reviewBody: {
+          seller: {
+            title: 'Продавець',
+            value: 'Machinery',
+          },
+          rating: {
+            title: 'Оцінка',
+            value: '5/5',
+          },
+          text: [
+            "Цей ноутбук має гарний дизайн та добре виконує свої основні функції. Швидкість роботи та продуктивність в цілому є на задовільному рівні. Крім того, присутній достатній обсяг пам'яті та потужність акумулятора.",
+            ' Однак, можливі проблеми з перегрівом під час тривалого використання, та може бути недостатньої ємності жорсткого диска для потреб користувача. Крім того, ціна може бути дещо високою порівняно з аналогічними моделями.',
+          ],
+          advantages: {
+            title: 'Переваги',
+            text: 'Дизайн, швидкість роботи, обсяг пам’яті, акумулятор',
+          },
+          disadvantages: {
+            title: 'Недоліки',
+            text: 'Перегрів, ємкість жосткого диску',
+          },
+          images: [
+            'https://picsum.photos/110/100',
+            'https://picsum.photos/110/100',
+            'https://picsum.photos/110/100',
+          ],
+          coments: [
+            {
+              userName: 'Користувач',
+              date: '1 січня 2023',
+              text: 'Дякую за відгук, я подумаю про недоліки, про які ви згадували, перш ніж зробити покупку.',
+            },
+            {
+              userName: 'Користувач',
+              date: '3 січня 2023',
+              text: 'Дякую за корисний відгук! Це допомогло мені зробити вибір щодо покупки ноутбука.',
+            },
+            {
+              userName: 'Користувач',
+              date: '10 січня 2023',
+              text: 'Дякую за відгук, але я хотів би дізнатися більше деталей про проблеми з перегрівом, щоб зробити зважений вибір перед покупкою.',
+            },
+          ],
+        },
+      },
+      {
+        title: {
+          userName: 'Петро Петров',
+          caption: 'Відгук покупця',
+          date: '1 січня 2023',
+        },
+        reviewBody: {
+          seller: {
+            title: 'Продавець',
+            value: 'Machinery',
+          },
+          rating: {
+            title: 'Оцінка',
+            value: '5/5',
+          },
+          text: [
+            "Цей ноутбук має гарний дизайн та добре виконує свої основні функції. Швидкість роботи та продуктивність в цілому є на задовільному рівні. Крім того, присутній достатній обсяг пам'яті та потужність акумулятора.",
+          ],
+          advantages: {
+            title: 'Переваги',
+            text: 'Обсяг пам’яті, акумулятор',
+          },
+          disadvantages: {
+            title: 'Недоліки',
+            text: 'Не виявлено',
+          },
+          images: ['https://picsum.photos/110/100'],
+          coments: [
+            {
+              userName: 'Користувач',
+              date: '1 січня 2023',
+              text: 'Дякую за відгук, я подумаю про недоліки, про які ви згадували, перш ніж зробити покупку.',
+            },
+          ],
+        },
+      },
+    ],
+
+    service: {
+      title: 'Our Services',
+      description:
+        'We offer a variety of services to meet your needs, including web design, content creation, and social media management.',
       buttons: [
         {
-          text: 'Редагувати',
+          text: 'Show More',
           link: 'https://www.youtube.com/',
-          isPrimary: true,
-        },
-        {
-          text: 'Очистити',
-          link: 'https://www.youtube.com/',
-          isSecondary: true,
-        },
-        {
-          text: 'Видалити',
-          link: 'https://www.youtube.com/',
-          isDanger: true,
         },
       ],
-      recipients: {
-        title: 'Отримувачі',
-        users: [
-          {
-            name: 'Іванов Іван Іванович',
-            phone: '+38 (098) 222 22 22',
-            button: {
-              text: 'Редагувати',
-              link: 'https://www.youtube.com/',
-            },
-          },
-          {
-            name: 'Петров Петро Петрович',
-            phone: '+38 (098) 111 11 11',
-            button: {
-              text: 'Редагувати',
-              link: 'https://www.youtube.com/',
-            },
-          },
-        ],
-      },
-      contacts: {
-        title: 'Контакти',
-        contactData: [
-          {
-            title: 'Номер телефону',
-            value: '+38 (098) 222 22 22',
-            button: {
-              text: 'Редагувати',
-              link: 'https://www.youtube.com/',
-              isPrimary: true,
-            },
-          },
-          {
-            title: 'Додатковий номер телефону',
-            value: '+38 (098) 111 11 11',
-            button: {
-              text: 'Очистити',
-              link: 'https://www.youtube.com/',
-              isSecondary: true,
-            },
-          },
-          {
-            title: 'Електронна адреса',
-            value: 'ivanivanov@ukr.net',
-            button: {
-              text: 'Видалити',
-              link: 'https://www.youtube.com/',
-              isDanger: true,
-            },
-          },
-        ],
-      },
     },
 
     footer: [
